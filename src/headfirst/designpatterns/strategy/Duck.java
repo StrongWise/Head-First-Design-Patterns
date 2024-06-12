97da275826e94380111b3ab714730305d5bdf92e
StrongWise <ghoul300@naver.com>
2024-06-12 15:51:51 +0900

    feat: add duck instance variable

--- src/headfirst/designpatterns/strategy/Duck.java
+++ src/headfirst/designpatterns/strategy/Duck.java
@@ -4,6 +4,15 @@ package headfirst.designpatterns.strategy;
  * description :
  */
 public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+    public void performQuack() {
+        quackBehavior.quack();
+    }
     public void quack() {
 		System.out.println("Quack");
     }
--- src/headfirst/designpatterns/strategy/MallardDuck.java
+++ src/headfirst/designpatterns/strategy/MallardDuck.java
@@ -4,8 +4,13 @@ package headfirst.designpatterns.strategy;
  * description :
  */
 public class MallardDuck extends Duck implements QuackBehavior, FlyBehavior {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
     public void display() {
-        System.out.println("I'm a real Mallard duck");
+        System.out.println("저는 물오리입니다");
     }
     public void fly() {
 		System.out.println("I'm flying!!");
