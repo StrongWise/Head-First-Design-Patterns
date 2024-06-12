dfe00f270d032f96350d045109c8aadc60542c87
StrongWise <ghoul300@naver.com>
2024-06-12 16:04:56 +0900

    feat: add behavior setter

--- src/headfirst/designpatterns/strategy/Duck.java
+++ src/headfirst/designpatterns/strategy/Duck.java
@@ -7,19 +7,26 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
-    public void performFly() {
-        flyBehavior.fly();
-    }
-    public void performQuack() {
-        quackBehavior.quack();
-    }
-    public void quack() {
-		System.out.println("Quack");
-    }
-
     public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
 
     abstract void display();
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
 }
