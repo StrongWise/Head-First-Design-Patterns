498ee17a9d5754d3f74914f8afbcb79d57cb75de
StrongWise <ghoul300@naver.com>
2024-06-17 17:22:33 +0900

    refactor: lambdas

--- src/headfirst/designpatterns/observer/swing/SwingObserverExample.java
+++ src/headfirst/designpatterns/observer/swing/SwingObserverExample.java
@@ -19,8 +19,8 @@ public class SwingObserverExample {
         frame = new JFrame();
 
         JButton button = new JButton("할까? 말까?");
-        button.addActionListener(new AngelListener());
-        button.addActionListener(new DevilListener());
+        button.addActionListener(e -> System.out.println("하지 마! 아마 후회할 걸?"));
+        button.addActionListener(e -> System.out.println("그냥 저질러 버렸!!!"));
 
         frame.getContentPane().add(BorderLayout.CENTER, button);
 
@@ -28,16 +28,5 @@ public class SwingObserverExample {
         frame.getContentPane().add(BorderLayout.CENTER, button);
         frame.setSize(300, 300);
         frame.setVisible(true);
-    }
-
-    class AngelListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("하지 마! 아마 후회할 걸?");
-        }
-    }
-    class DevilListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("그냥 저질러 버렸!!!");
-        }
     }
 }
