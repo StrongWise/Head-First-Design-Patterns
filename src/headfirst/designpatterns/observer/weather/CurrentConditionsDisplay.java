74b222769b5424ebc4f23c8cebcc89ebaee3c61f
StrongWise <ghoul300@naver.com>
2024-06-17 16:22:08 +0900

    feat: weather station Display implements

--- src/headfirst/designpatterns/observer/weather/CurrentConditionsDisplay.java
+++ src/headfirst/designpatterns/observer/weather/CurrentConditionsDisplay.java
@@ -3,8 +3,23 @@ package headfirst.designpatterns.observer.weather;
 /**
  * description :
  */
-public class CurrentConditionsDisplay {
-    public void update(float temperature, float humidity, float pressure) {
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
     }
 
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("현재 상태: 온도 " + temperature + "F, 습도 " + humidity + "%");
+    }
 }
