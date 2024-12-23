38babff20ca0a575cd6fa75e4a16e11575e119ae
StrongWise <kanghyunkim01@gmail.com>
2024-12-23 13:28:17 +0900

    feat: DCL Singleton

---
+++ src/headfirst/designpatterns/singletion/dcl/SingletonClient.java
@@ -0,0 +1,11 @@
+package headfirst.designpatterns.singletion.dcl;
+
+/**
+ * description :
+ */
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+}
