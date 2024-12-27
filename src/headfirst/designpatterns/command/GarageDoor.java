6c1f978d8e0f51ca6d152cf03328cb7d97fc2de3
StrongWise <kanghyunkim01@gmail.com>
2024-12-27 16:25:00 +0900

    feat: add GarageDoorOpenCommand

---
+++ src/headfirst/designpatterns/command/GarageDoor.java
@@ -0,0 +1,30 @@
+package headfirst.designpatterns.command;
+
+/**
+ * description :
+ */
+public class GarageDoor {
+    public GarageDoor() {
+    }
+
+	public void up() {
+		System.out.println("garage Door is Up");
+	}
+
+	public void down() {
+		System.out.println("garage Door is Down");
+	}
+
+	public void stop() {
+		System.out.println("garage Door is Stopped");
+	}
+
+	public void lightOn() {
+		System.out.println("garage light is on");
+	}
+
+	public void lightOff() {
+		System.out.println("garage light is off");
+	}
+
+}
---
+++ src/headfirst/designpatterns/command/GarageDoorOpenCommand.java
@@ -0,0 +1,16 @@
+package headfirst.designpatterns.command;
+
+/**
+ * description :
+ */
+public class GarageDoorOpenCommand implements Command {
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    public void execute() {
+        garageDoor.up();
+    }
+}
